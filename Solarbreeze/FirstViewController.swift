0e5fd22e930f65ead47bb7f74d8a0f35d5ac35c2
Eric Betts <bettse@fastmail.fm>
2016-05-27 19:35:17 -0700

    On switch

--- Solarbreeze/FirstViewController.swift
+++ Solarbreeze/FirstViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    @IBOutlet weak var onSwitch: UISwitch!
 
     let fakeBase = FakeBase.singleton
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        onSwitch.addTarget(self, action: #selector(self.firmwareSwitch), forControlEvents: .ValueChanged)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -22,6 +24,14 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func firmwareSwitch() {
+        //Disable idle timer when fakebase is on
+        UIApplication.sharedApplication().idleTimerDisabled = onSwitch.on
+        if onSwitch.on {
+            fakeBase.start()
+        } else {
+            fakeBase.stop()
+        }
+    }
 }
 
