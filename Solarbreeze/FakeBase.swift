721107c892f536c39096ef2e4cec34d4c5f83eb8
Eric Betts <bettse@fastmail.fm>
2016-05-28 16:50:13 -0700

    Starting to refocus on protocol

--- Solarbreeze/FakeBase.swift
+++ Solarbreeze/FakeBase.swift
@@ -14,8 +14,8 @@ class FakeBase {
     let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
     let bleInterface = FakeBaseInterface()
     
-    static var corrolationGenerator = (1..<UInt8.max-1).generate()
-    static var nextSequence : UInt8 {
+    var corrolationGenerator = (1..<UInt8.max-1).generate()
+    var nextSequence : UInt8 {
         get {
             if let next = corrolationGenerator.next() {
                 return next
@@ -25,6 +25,18 @@ class FakeBase {
             }
         }
     }
+    
+    //Status is 16 pairs of bits
+    //high bit shows...
+    //low bit shows...
+    //coming = 
+    //going = 
+    //still here = 
+    //not here = 00
+    var currentStatus : UInt32 = 0;
+    var nextStatus : UInt32 = 0;
+    
+    let onPortal : [Token] = [Token]()
     
     init() {
         bleInterface.registerIncomingReportCallback(incomingReport)
@@ -38,6 +50,16 @@ class FakeBase {
         bleInterface.stop()
     }
 
+    
+    func placeToken(newToken: Token) {
+        //Sparse array in swift?
+        //Add to nextStatus
+    }
+    
+    func removeToken(oldToken: Token) {
+        
+    }
+    
     func incomingReport(report: NSData) {
         var response : NSData = NSData()
         print("Command \(Character(UnicodeScalar(report[0])))")
@@ -57,7 +79,9 @@ class FakeBase {
             response = NSData(bytes: [report[0], 0x02, 0x19] as [UInt8], length: 3)
             break;
         case "S".asciiValue:
-            response = NSData(bytes: [report[0], 0x00, 0x00, 0x00, 0x00, FakeBase.nextSequence, 0x01, 0xaa, 0x86, 0x02, 0x19] as [UInt8], length: 11)
+            //shift nextStatus and OR into currentdStatus
+            response = NSData(bytes: [report[0], 0x00, 0x00, 0x00, 0x00, nextSequence, 0x01, 0xaa, 0x86, 0x02, 0x19] as [UInt8], length: 11)
+            //copy nextStatus into currentStatus, zero out nextStatus
             break;
         case "W".asciiValue:
             response = NSData(bytes: [report[0], report[1], report[2]] as [UInt8], length: 3)
